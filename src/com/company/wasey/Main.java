94b92df210b2fa09642cc8bd87f065db8f6bcac2
abdulwaseybutt <abdulwaseybutt@gmail.com>
2020-11-06 19:30:11 +0500

    for loop

--- src/com/company/wasey/Main.java
+++ src/com/company/wasey/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("conflict");
+        for (int i = 0; i < 50 ; i++){
+
+        }
     }
 }
